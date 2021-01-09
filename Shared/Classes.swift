af5d9251c3c0ce7b8bf17c0a4b9ab38dc218de52
pradyun <pradyun.setti@gmail.com>
2021-01-09 20:01:04 +1300

    add classes and dashboard views

---
+++ Shared/Classes.swift
@@ -0,0 +1,21 @@
+//
+//  Classes.swift
+//  SchoolHelper
+//
+//  Created by Pradyun Setti on 9/01/21.
+//
+
+import SwiftUI
+
+struct Classes: View {
+    
+    var body: some View {
+        Text("temp")
+    }
+}
+
+struct Classes_Preview: PreviewProvider {
+    static var previews: some View {
+        Classes()
+    }
+}
---
+++ Shared/Dashboard.swift
@@ -0,0 +1,21 @@
+//
+//  Dashboard.swift
+//  SchoolHelper
+//
+//  Created by Pradyun Setti on 9/01/21.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    
+    var body: some View {
+        Text("temp")
+    }
+}
+
+struct Dashboard_Previews: PreviewProvider {
+    static var previews: some View {
+        Dashboard()
+    }
+}
