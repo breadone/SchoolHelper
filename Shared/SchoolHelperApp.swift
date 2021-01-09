722641cbd415bbc0ab589a5b1ef838f078f00015
pradyun <pradyun.setti@gmail.com>
2021-01-09 19:51:45 +1300

    Initial Commit

---
+++ Shared/SchoolHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolHelperApp.swift
+//  Shared
+//
+//  Created by Pradyun Setti on 9/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SchoolHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
