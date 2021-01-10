f8cffcd1e7882d310edc17c505837a9369c039a4
pradyun <pradyun.setti@gmail.com>
2021-01-10 23:31:33 +1300

    -attempted date to string conversion but uh not successful -added sorting for task list items

--- Shared/Tasks.swift
+++ Shared/Tasks.swift
@@ -10,17 +10,18 @@ import CoreData
 
 struct Tasks: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: TaskItem.entity(), sortDescriptors: []) var tasks: FetchedResults<TaskItem>
+    @FetchRequest(entity: TaskItem.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \TaskItem.dateCreated, ascending: false)]) var tasks: FetchedResults<TaskItem>
     
     @State private var showingAddScreen = false
-    
+    @State private var noDueDate = false
+
     var body: some View {
         NavigationView {
             List {
-                ForEach(tasks, id: \.self) { task in
+                ForEach(tasks, id: \.self) { task in                    
                     HStack {
-                        Text("dueDate")
-                        
+//                        Text(noDueDate ? "" : DateToString(task.dueDate!))
+                       Text("08/10")
                         NavigationLink(
                             destination: Text(task.desc ?? "no title")) {
                             Text(task.name ?? "no description")
@@ -31,15 +32,22 @@ struct Tasks: View {
                 .navigationBarTitle("Tasks")
                 .navigationBarItems(trailing:
                     Button(action: {self.showingAddScreen.toggle()},
-                           label: {
-                            Image(systemName: "plus")
-                           })
+                           label: {Image(systemName: "plus")})
                 )
                 .sheet(isPresented: $showingAddScreen) {
                     AddTaskView().environment(\.managedObjectContext, self.moc)
                 }
         }
     }
+    
+    func DateToString(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM"
+        
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
+    
 }
 
 struct Tasks_Previews: PreviewProvider {
